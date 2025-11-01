1aa48c0afbbbc410ef5d97d88694608e2f81f2eb
Chris Brouwer <chris.brouwer@gmail.com>
2025-11-01 10:47:45 +0100

    Day 9 part 2

--- day_9.kt
+++ day_9.kt
@@ -25,74 +25,76 @@ fun part1() {
             0
         }
     }
-    println(sum)
+    println("Part 1: $sum")
 
    
 }
 
-// fun part1_old() {
-//     val input = getInput()
-
-//     val actualInput = if (input.size %2 == 0) input else input + 0
-
-    
-//     val (evenWithOrigIndex, unevenWithOrigIndex) = actualInput.withIndex().partition { it.index % 2 == 0}
-    
-//     val result: List<DiskFile> = evenWithOrigIndex.zip(unevenWithOrigIndex).flatMapIndexed { fileIdx, (eventValueWithIdx, unevenValueWithIdx) -> 
-//         val list1: List<DiskFile> = List(eventValueWithIdx.value) { _ -> DiskFile(fileIdx.toLong()) }
-//         val list2: List<DiskFile> = List(unevenValueWithIdx.value) { _ -> DiskFile(-1L) }
-//         list1 + list2
-//     }
-
-    
-//     //(result.toDiskString())
-
-//     val updatedResult = result.toMutableList()
-//     while (!hasGaps(updatedResult) ) {
-//         switchAround(updatedResult)
-//     }
-//   //  println(updatedResult.toDiskString())
-//     println(checksum(updatedResult))
-   
-// }
-
-// fun hasGaps(list: List<DiskFile>): Boolean {
-//     val idx1 = list.indexOfFirst { it.isEmpty() }
-//     val idx2 = list.indexOfLast {  !it.isEmpty()}
-//     return idx1 == idx2 +1
-// }
-
-// fun switchAround(list: MutableList<DiskFile>) {
-
-//     val lastCharIdx = list.indexOfLast { !it.isEmpty() }
-//     val firstEmptyIdx = list.indexOfFirst {it.isEmpty()}
-    
-//     val emptyFile = list.get(firstEmptyIdx)
-//     val fileToSwitch = list.get(lastCharIdx)
-
-//     list.set(firstEmptyIdx, DiskFile(fileToSwitch.fileId))
-//     list.set(lastCharIdx, DiskFile(-1))
-// }
 
 fun part2() {
     val input = getInput()
+
+    val disk : List<Long?> = input.windowed(2, 2, true).withIndex().flatMap { (idx, value) -> 
+        List(value[0]) { _ -> idx.toLong() } + 
+        (value.getOrNull(1)?.let { List(it) { _ -> null }} ?: emptyList() )
+    }
+   
+    var currentNumber: Long? = null
+    var currentSize: Int? = null
+
+    val optimizedDisk = disk.toMutableList()
+
+    disk.withIndex().reversed().forEach { (idx, elem) ->
+         if (elem != null && currentNumber == null) {
+            currentNumber = elem
+            currentSize = 1
+         } else if (elem != null && currentNumber != null && elem == currentNumber) {
+            currentSize = currentSize!! +  1
+         } else if (elem != currentNumber) {
+            val openSpotIdx = findEmptySpotOfSize(currentSize!!, optimizedDisk, idx)
+            if (openSpotIdx != null) {
+                for (updateIdx in 0..currentSize-1) { 
+                    optimizedDisk[openSpotIdx + updateIdx] = currentNumber
+                     
+                    // Offset by one since idx is already pointing at the next number
+                    optimizedDisk[idx+updateIdx+1] = null
+                }
+            } 
+            // Finished moving, or unable to
+            currentNumber = elem
+            currentSize = 1
+        }
+    }
+     val newSum = optimizedDisk.withIndex().sumOf{ (idx, value) -> 
+        value?.times(idx) ?: 0
+    }
+   
+    println("Part 2: $newSum")
+
 }
 
+fun findEmptySpotOfSize(size: Int, disk: List<Long?>, maxIdx: Int): Int?  {
+    return disk.indices.firstOrNull { 
+        if (it > maxIdx) {
+            false
+        } else {
+            val allNull = (0..size-1).all{  i ->
+                if (it+i > disk.size-1) {
+                    false 
+                }else {
+                    disk[it+i] == null
+                }
+            
+            }
+            if (allNull) {
+                true
+            } else {
+                false
+            }
+        }
+    }
+    
+}
+
+
 fun getInput(): List<Int> = File("day_9_input.txt").readLines().first().map{ it.digitToInt() }
-
-// data class DiskFile(val fileId: Long) {
-//     fun isEmpty(): Boolean = this.fileId == -1L
-// }
-// fun List<DiskFile>.toDiskString(): String {
-//     val sb = StringBuilder()
-//     this.forEach{ file ->
-//         if (file.fileId < 0) sb.append(".") else sb.append("${file.fileId}")
-//     }
-//     return sb.toString()
-// }
-
-// fun checksum(input: List<DiskFile>): Long {
-//     return input.withIndex().filterNot{it.value.isEmpty()}.sumOf { (idx, file) ->
-//         idx * file.fileId
-//     }
-// }
